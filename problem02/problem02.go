3799595d000eb3c128db47cdcf0bfabf4b00166a
John Peña <john@getcorrelated.com>
2021-12-02 09:21:57 -0500

    finished problem 2

--- problem02/problem02.go
+++ problem02/problem02.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/johnpena/go-aoc-2021/util"
@@ -17,11 +18,51 @@ func main() {
 }
 
 func first() {
+	position := 0
+	depth := 0
 	for _, line := range util.Lines(problem02input) {
 		parts := strings.Split(line, " ")
-		fmt.Println(parts)
+		direction := parts[0]
+		magnitude, err := strconv.Atoi(parts[1])
+		util.Check(err)
+
+		switch direction {
+		case "forward":
+			position += magnitude
+		case "down":
+			depth += magnitude
+		case "up":
+			depth -= magnitude
+		default:
+			panic(fmt.Sprintf("bad direction %s", direction))
+		}
 	}
+
+	fmt.Println(position * depth)
 }
 
 func second() {
+	position := 0
+	depth := 0
+	aim := 0
+	for _, line := range util.Lines(problem02input) {
+		parts := strings.Split(line, " ")
+		direction := parts[0]
+		magnitude, err := strconv.Atoi(parts[1])
+		util.Check(err)
+
+		switch direction {
+		case "forward":
+			position += magnitude
+			depth += aim * magnitude
+		case "down":
+			aim += magnitude
+		case "up":
+			aim -= magnitude
+		default:
+			panic(fmt.Sprintf("bad direction %s", direction))
+		}
+	}
+
+	fmt.Println(position * depth)
 }
